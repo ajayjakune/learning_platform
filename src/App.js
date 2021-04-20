c53f2976c58ac0ceeadc2edd6910d3919d5b4da8
Gopal <gop>
2021-04-20 10:24:19 +0530

    Added Videos and Tabs to course Enrolled Page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { HashRouter } from 'react-router-dom';
-// import { Container } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import Body from './components/body';
 
 function App() {
