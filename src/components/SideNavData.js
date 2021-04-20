7c9d43a46e99f8dee69f48cba4b35856fdc3bb10
ShivamVerma-15 <shivam_verma@persistent.co.in>
2021-04-20 17:21:45 +0530

    Improving UI and adding sidebar component

--- src/components/SideNavData.js
+++ src/components/SideNavData.js
@@ -3,14 +3,17 @@
 export const SideNavData = [
     {
         title: 'Chapter 1',
+        id: 1,
         lectures: ['lecture 1', 'lecture 2', 'lecture 3' ]
     },
     {
         title: 'Chapter 2',
+        id: 2,
         lectures: ['lecture 1', 'lecture 2', 'lecture 3' ]
     },
     {
         title: 'Chapter 3',
+        id: 3,
         lectures: ['lecture 1', 'lecture 2', 'lecture 3' ]
     },
 ]
