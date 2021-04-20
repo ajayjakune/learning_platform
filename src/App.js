aed68faff963956ceda504840538e0f033ed8477
Gopal <gop>
2021-04-20 10:24:19 +0530

    Added Videos and Tabs to course Enrolled Page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import { HashRouter } from 'react-router-dom';
-// import { Container } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import Body from './components/body';
 
 
 function App() {
   return (
     <HashRouter>
-        {/* <Header /> */}
-        <Body />
-        {/* <Footer /> */}
+      {/* <Header /> */}
+      <Body />
+      {/* <Footer /> */}
     </HashRouter >
   );
 }
--- src/components/sample.jsx
+++ src/components/sample.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+
 class CommentBox extends React.Component {
     constructor() {
         super();
