e7c14a2851c3ab8cdc1da3301e86724a12b46df5
Gopal <gop>
2021-04-21 16:07:51 +0530

    Removed Component Rendering from App.js for Merging with main

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
+
+import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
-import Body from './components/course_enrollment_component/body';
 
 function App() {
   return (
-    <BrowserRouter>
-      {/* <Header /> */}
-      <Body />
-      {/* <Footer /> */}
-    </BrowserRouter >
-  );
+    
+  )
 }
 
 export default App;
