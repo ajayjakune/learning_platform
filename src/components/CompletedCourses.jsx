0c7d4557da9cf59240ca14d5dd2caaf0763ff1df
SanilShah <sanil_shah@persistent.com>
2021-04-21 12:41:05 +0530

    Update CompletedCourses.jsx

--- src/components/CompletedCourses.jsx
+++ src/components/CompletedCourses.jsx
@@ -6,9 +6,9 @@ import { CourseData } from "../data/CourseData";
 
 const renderCard = (card, index) => {
         return (
-            <div class="col-sm-3 justify-content-center" style={{paddingInline: 70, paddingBlock: 20}}>
-            <Card style={{ width: '18rem', borderRadius:18}} key={index}>
-                <Card.Img variant="top" src={card.Photo} style={{height: '15rem'}}/>               
+            <div class="col-lg-3 col-md-6">
+            <Card  key={index}>
+                <Card.Img variant="top" src={card.Photo} style={{height: '15rem', padding: "20px"}}/>               
                 <Card.Body>
                     <Card.Title>{card.Name}</Card.Title>
                     <Card.Text>
@@ -34,9 +34,12 @@ class CompletedCourses extends Component {
     state = {};
     render() {
         return (
-            <div class="row justify-content-left">
-                {CourseData.map(renderCard)}
-            </div>          
+            <div class="container">
+                <div class="row justify-content-center">
+
+                    {CourseData.map(renderCard)}
+                    </div>
+            </div>               
         );
     }
 }
