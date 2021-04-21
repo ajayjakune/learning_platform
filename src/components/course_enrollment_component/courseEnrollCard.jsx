d2a00a9c35635343653fd27006eed5cab402d09a
ajayjakune <jakuneajay143@gmail.com>
2021-04-21 12:19:15 +0530

    handled state change in course enrollment page

--- src/components/course_enrollment_component/courseEnrollCard.jsx
+++ src/components/course_enrollment_component/courseEnrollCard.jsx
@@ -2,6 +2,19 @@ import React, { Component } from 'react'
 import './css/courseEnrollCard.css';
 
 export class CourseEnrollCard extends Component {
+    constructor() {
+        super();
+        this.state = { btn: "Enroll" };
+    }
+
+    handleChange() {
+        if (this.state.btn === "Enroll") {
+            this.setState({ btn: "Go to course" });
+        }
+        else if (this.state.btn === "Go to course") {
+            alert("Already Registerd");
+        }
+    }
     render() {
         return (
             <div className="card card-size float-card sticky">
@@ -14,7 +27,7 @@ export class CourseEnrollCard extends Component {
                 <div className="card-body">
                     <h5 className="card-title">React JS</h5>
                     <p className="card-text">Enroll for react js course and make your career grow</p>
-                    <a href="#" className="btn btn-primary button-center">&nbsp;&nbsp;&nbsp;Enroll&nbsp;&nbsp;&nbsp;</a>
+                    <button className="btn btn-primary button-center" onClick={this.handleChange.bind(this)}>{this.state.btn}</button>
                 </div>
             </div>
         )
