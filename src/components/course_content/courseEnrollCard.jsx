95348b8ea4b02c01af58bbbabcdbf6174f7b4432
spushkaraj <spasco48-dev@yahoo.com>
2021-04-24 20:13:25 +0530

    course content api integrated

--- src/components/course_content/courseEnrollCard.jsx
+++ src/components/course_content/courseEnrollCard.jsx
@@ -1,30 +1,21 @@
-import React, { Component } from 'react'
+import React,{useState} from 'react'
 import './css/courseEnrollCard.css';
 import { Link } from 'react-router-dom';
 
-export class CourseEnrollCard extends Component {
-    constructor() {
-        super();
-        this.state = { btn: "Enroll" };
-    }
 
-    handleChange() {
-        if (this.state.btn === "Enroll") {
-            this.setState({ btn: "Go to course" });
-        }
-    }
-    render() {
-        return (
-            <div className="card card-size float-card sticky">
-                <img className="card-img-top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png" alt="course_img" />
-                <div className="card-body">
-                    <h5 className="card-title">React JS</h5>
-                    <p className="card-text">Enroll for react js course and make your career grow</p>
-                    <Link to="/" className="btn btn-primary button-center" onClick={this.handleChange.bind(this)}>{this.state.btn}</Link>
-                </div>
+const CourseEnrollCard = (props) => {
+
+    const [btnText, setBtnText] = useState('Enroll for the Course');
+
+    return (
+        <div className="card card-size float-card sticky">
+            <img className="card-img-top" src={`${props.courseImg}`} alt="course" />
+            <div className="card-body">
+                <h5 className="card-title text-center">{props.courseTitle}</h5>
+                <Link to="/" className="btn btn-primary" onClick={() => btnText === 'Enroll for the Course' ? setBtnText('Go to course') : setBtnText('Enroll for the Course') }>{btnText}</Link>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
-export default CourseEnrollCard;
+export default CourseEnrollCard
