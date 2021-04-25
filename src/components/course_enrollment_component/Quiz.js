06365e663858034c3e25307720c649353c945f46
Ashutosh jawanjal <47347235+Ashu151199@users.noreply.github.com>
2021-04-25 22:21:32 +0530

    Added network request for posting user details to database.

--- src/components/course_enrollment_component/Quiz.js
+++ src/components/course_enrollment_component/Quiz.js
@@ -3,40 +3,53 @@ import { Button } from 'react-bootstrap';
 import QuizResultComponent from './QuizResult';
 
 export default function Quiz(props) {
-	const questions = props.questions;
-	const [currentQuestion, setCurrentQuestion] = useState(0);
-	const [showScore, setShowScore] = useState(false);
-	const [score, setScore] = useState(0);
-	const [toggle, setToggle] = useState(false)
-	const [result, setResult] = useState(false);
+  const questions = props.questions;
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
+  const [toggle, setToggle] = useState(false);
+  const [result, setResult] = useState(false);
 
+  const handleAnswerOptionClick = (answer) => {
+    if (questions[currentQuestion].correct_answer === answer) {
+      setScore(score + 1);
+    }
 
-	const handleAnswerOptionClick = (answer) => {
-		if (questions[currentQuestion].correct_answer === answer) {
-			setScore(score + 1);
-		}
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  };
 
-		const nextQuestion = currentQuestion + 1;
-		if (nextQuestion < questions.length) {
-			setCurrentQuestion(nextQuestion);
-		} else {
-			setShowScore(true);
-		}
-	};
+  const checkScoreHandler = (score) => {
+    if (score >= questions.length / 2) {
+      setToggle(true);
+      setResult(true);
+      // setScore(0)
+    } else {
+      setToggle(true);
+      setResult(false);
+      // setScore(0)
+    }
 
-	const checkScoreHandler = score => {
-		if (score >= questions.length / 2) {
-			setToggle(true)
-			setResult(true)
-			// setScore(0)
-		}
-		else {
-			setToggle(true)
-			setResult(false)
-			// setScore(0)
-		}
-	}
-	/*
+    //course id of course for which test is to be conducted
+    const courseId = '608271ce0ffb371d38b95daf';
+    fetch(`http://localhost:5000/${courseId}/test`, {
+      method: 'put',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        isPassed: result,
+        score: score,
+      }),
+    })
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  };
+  /*
 		
 		dataFromResponse[0].questions structure:- Array of questions
 			dataFromResponse[0].questions[0] structure :- object
@@ -48,38 +61,47 @@ export default function Quiz(props) {
 			dataFromResponse[0].questions[1] structure :- object
 						same as above
 	*/
-	return (
-		<div className="quiz-main text-center">
-			<h2>Final Quiz</h2>
-			<div className='quiz-card'>
-				{showScore ? (
-					<div className='score-section'>
-						<Button variant="primary" onClick={() => checkScoreHandler(score)}>Check your score</Button>
-						<div>
-							{toggle === true
-								? <QuizResultComponent res={result} score={score} quesLength={questions.length} />
-								: null
-							}
-						</div>
-					</div>
-				) : (
-					<>
-						<div className='question-section'>
-							<div className='question-count'>
-								<span>Question {currentQuestion + 1}</span>/{questions.length}
-							</div>
-							<div className='question-text'>{questions[currentQuestion].question}</div>
-						</div>
-						<div className='answer-section'>
-							{questions[currentQuestion].options.map((answerOption, index) => (
-								<div key={index}>
-									<button onClick={() => handleAnswerOptionClick(answerOption)}>{index + 1}. {answerOption}</button>
-								</div>
-							))}
-						</div>
-					</>
-				)}
-			</div>
-		</div>
-	);
+  return (
+    <div className='quiz-main text-center'>
+      <h2>Final Quiz</h2>
+      <div className='quiz-card'>
+        {showScore ? (
+          <div className='score-section'>
+            <Button variant='primary' onClick={() => checkScoreHandler(score)}>
+              Check your score
+            </Button>
+            <div>
+              {toggle === true ? (
+                <QuizResultComponent
+                  res={result}
+                  score={score}
+                  quesLength={questions.length}
+                />
+              ) : null}
+            </div>
+          </div>
+        ) : (
+          <>
+            <div className='question-section'>
+              <div className='question-count'>
+                <span>Question {currentQuestion + 1}</span>/{questions.length}
+              </div>
+              <div className='question-text'>
+                {questions[currentQuestion].question}
+              </div>
+            </div>
+            <div className='answer-section'>
+              {questions[currentQuestion].options.map((answerOption, index) => (
+                <div key={index}>
+                  <button onClick={() => handleAnswerOptionClick(answerOption)}>
+                    {index + 1}. {answerOption}
+                  </button>
+                </div>
+              ))}
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
