92ffd10026829d07460cce3304e0abb5293cf7e3
spushkaraj <spasco48-dev@yahoo.com>
2021-04-29 14:31:52 +0530

    gopal query resolved

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -114,7 +114,7 @@ const About = () => {
             <div class="row custom-container" style={{ marginBottom: "2vh"}}>
             <div class="col-lg-2 col-md-6 col-12 mt-4 pt-2">
                 <div class="team text-center rounded p-3 py-4 content-card cus-height">
-                    <img src="/aboutUs/be2.jpeg" class="img-fluid avatar avatar-medium shadow rounded-pill" alt=""></img>
+                    <img src="/aboutUs/fe7.jpeg" class="img-fluid avatar avatar-medium shadow rounded-pill" alt=""></img>
                     <div class="content mt-3">
                         <h4 class="title mb-0">Vaibhav Jadhav</h4>
                         <small class="text-muted">Frontend Team Member</small>
